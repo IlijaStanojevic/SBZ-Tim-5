f21f8128e7371a2e75eb2d250f9fe234d3e86182
REX1389 <rrvlelcc@gmail.com>
2024-05-15 19:26:50 +0200

    backend setup

---
+++ SBZ-backend/src/main/java/com/example/sbzbackend/SbzBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbzbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbzBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbzBackendApplication.class, args);
+	}
+
+}
---
+++ SBZ-backend/src/test/java/com/example/sbzbackend/SbzBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbzbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbzBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
